30390d1978cded25fcdf6eaa4d090a3b3b64185b
hvdwolf <hvdwolf@gmail.com>
2015-05-01 14:32:31 +0200

    Update download_all_wikivoyagedumps.sh

--- scripts/download_all_wikivoyagedumps.sh
+++ scripts/download_all_wikivoyagedumps.sh
@@ -3,7 +3,9 @@
 # bash file to download all available wikidumps
 # version 0.1, Harry van der Wolf, 20150419
 
-languages="ar ca cs da de el en eo es et eu fa fi fr gl he hi hr hu it ja ko lt ms nl nn no pl pt ro ru simple sk sl sr sv te tr uk vi vo zh" 
+#languages="ar ca cs da de el en eo es et eu fa fi fr gl he hi hr hu it ja ko lt ms nl nn no pl pt ro ru simple sk sl sr sv te tr uk vi vo zh" 
+# There are only a limited number of wikivoyage dumps available
+languages="de el en es fa fr he it nl pl pt ro ru sv uk vi zh" 
 
 for language in $languages
 do
