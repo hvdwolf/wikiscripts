2e3a81d3f31627b18980c96d1afd3ea26df0f6f0
hvdwolf <hvdwolf@gmail.com>
2015-05-05 11:28:13 +0200

    update a couple of scripts

--- scripts/download_parse_externallinks.sh
+++ scripts/download_parse_externallinks.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # bash file to download and parse all available external links dumps
-# version 0.1, Harry van der Wolf, 20150426
+# version 0.2, Harry van der Wolf, 20150505
 
 printf "Downloading language $1\n\n"
-python ../scripts/wikiextlinksdownloader.py $1
+wget -c http://dumps.wikimedia.org/${1}wiki/latest/${1}wiki-latest-pages-articles.xml.bz2
 printf "Done downloading $1."
 python ../scripts/externallinks.py $1
-#rm ${1}wiki-latest-externallinks.sql.gz
+rm ${1}wiki-latest-externallinks.sql.gz
 done
