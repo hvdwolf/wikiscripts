d78d95d37614105502d14f84222e0b9db67ded59
hvdwolf <hvdwolf@gmail.com>
2015-05-05 22:15:26 +0200

    Update clean_csv.sh

--- scripts/clean_csv.sh
+++ scripts/clean_csv.sh
@@ -9,13 +9,18 @@
 for i in "${@}"
 do
   # do some reformating like the &lt; to < and &gt; to >
-  printf "Some additional cleaning of the csv files"
+  printf "Some additional cleaning of the csv files\n"
   sed -e 's+&amp;+&+g' -e 's+&gt;+>+g' -e 's+&lt;+<+g' -e 's+nbsp;+ +g' ${i}_wikipedia.csv > ${i}_wikipedia2.csv
-  mv ${i}_wikipedia2.csv ${i}_wikipedia.csv
+  # Remove lines without text
+  grep -v '",""$' ${i}_wikipedia2.csv > ${i}_wikipedia.csv
+  # Remove intermediate nr2
+  rm ${i}_wikipedia2.csv
 
-printf "\n\n"
+  printf "\n\n"
 done
 
 # remove 0 byte files
 find -name '*.csv' -size 0 -delete
+
+
 
