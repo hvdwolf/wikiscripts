2cd1122a040b7a9dff3a55a2abbb269f9b5dd8bc
hvdwolf <hvdwolf@gmail.com>
2015-05-05 15:56:32 +0200

    Update download_parse_externallinks.sh

--- scripts/download_parse_externallinks.sh
+++ scripts/download_parse_externallinks.sh
@@ -4,7 +4,7 @@
 # version 0.2, Harry van der Wolf, 20150505
 
 printf "Downloading language $1\n\n"
-wget -c http://dumps.wikimedia.org/${1}wiki/latest/${1}wiki-latest-pages-articles.xml.bz2
+wget -c http://dumps.wikimedia.org/${1}wiki/latest/${1}wiki-latest-externallinks.sql.gz
 printf "Done downloading $1."
 python ../scripts/externallinks.py $1
 rm ${1}wiki-latest-externallinks.sql.gz
